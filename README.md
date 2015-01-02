3a4e39f41aac4866a72afde0ce2263ec3b0dd9c8
Florent Cailhol <florent.cailhol@gmail.com>
2015-01-02 15:45:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# speedr-array2
+
+> Two-dimensional array
+
+## Install
+
+```sh
+$ npm install --save speedr-array2
+```
+
+## Usage
+
+```js
+import {Array2} from 'speedr-array2'
+
+const Tiles = {
+  SAND: 0,
+  TREE: 1,
+  WALL: 2
+}
+
+// Constructors
+let map1 = new Array2(10, 10)
+let map2 = new Array2(80, 50, Tiles.SAND)
+let map3 = new Array2(40, 40, (x, y) => x * y)
+
+// Access
+map3.get(4, 9) // 36
+map3.set(4, 9, 12)
+```
+
+## License
+
+MIT © Florent Cailhol
---
+++ src/index.js
@@ -0,0 +1,25 @@
+export class Array2 {
+  constructor(w, h, initValue) {
+    this.w = w
+    this.h = h
+    this.e = new Array(w * h)
+
+    let fn = typeof initValue == 'function' ? initValue : () => initValue
+
+    for (let y = 0; y < h; y++) {
+      for (let x = 0; x < w; x++) {
+        this.set(x, y, fn(x, y))
+      }
+    }
+  }
+
+  get(x, y) {
+    if (x < 0 || y < 0 || x >= this.w || y >= this.h) return null
+    return this.e[y * this.w + x]
+  }
+
+  set(x, y, val) {
+    if (x < 0 || y < 0 || x >= this.w || y >= this.h) return
+    this.e[y * this.w + x] = val
+  }
+}
